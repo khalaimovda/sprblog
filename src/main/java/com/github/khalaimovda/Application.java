4e9ada2a3280fb22ffd0797d09b9526f7cfd1708
khalaimovda <halaimov_15@mail.ru>
2025-01-17 20:57:54 +0100

    MVC Initialization

--- src/main/java/com/github/khalaimovda/Application.java
+++ src/main/java/com/github/khalaimovda/Application.java
@@ -1,7 +1,11 @@
 package com.github.khalaimovda;
 
+import com.github.khalaimovda.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
